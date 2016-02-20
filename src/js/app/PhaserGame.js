2bc2cf020a626767d74c90895e5c7906e2d1a391
Javi Manzano <javi.manzano.oller@gmail.com>
2016-02-20 16:58:02 +0000

    Add the alien to the PhaserGame module

--- src/js/app/PhaserGame.js
+++ src/js/app/PhaserGame.js
@@ -1,4 +1,4 @@
-define(['Phaser', 'extensions/Spaceship'], function (Phaser) {
+define(['Phaser', 'extensions/Spaceship', 'extensions/Alien'], function (Phaser) {
 
     var PhaserGame = function (w, h, containerId) {
         return new Phaser.Game(w, h, Phaser.AUTO, containerId);
