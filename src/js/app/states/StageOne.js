66fb6df6b130b5ee85696d888a873ba06e790fa4
Javi Manzano <javi.manzano.oller@gmail.com>
2016-02-27 23:35:33 +0000

    Update the StageOne with the selection functionality

--- src/js/app/states/StageOne.js
+++ src/js/app/states/StageOne.js
@@ -3,12 +3,14 @@ define([
     'modules/Config',
     'modules/Print',
     'modules/Utils',
-    'modules/Collisions'
-], function (Phaser, Config, Print, Utils, Collisions) {
+    'modules/Collisions',
+    'modules/Selection'
+], function (Phaser, Config, Print, Utils, Collisions, Selection) {
 
     var game,
         config,
         collisions,
+        selection,
         spaceship,
         powerUps,
         aliens;
@@ -31,6 +33,8 @@ define([
             game.world.setBounds(0, 0, config.worldHeight, config.worldWidth);
             game.physics.startSystem(Phaser.Physics.ARCADE);
             game.starfield = game.add.sprite(0, 0, 'space');
+            game.starfield.inputEnabled = true;
+            game.selectedUnits = [];
 
             // Create spaceship
             spaceship = new Phaser.Spaceship(
@@ -40,30 +44,45 @@ define([
                 'spaceship',
                 0);
 
+            game.camera.focusOn(spaceship);
+
             // Create some power ups
             powerUps = Utils.spawnPowerUps(game, powerUps, config);
 
             // Create some aliens
             aliens = Utils.spawnAliensInGame(game, aliens, config);
 
-            // The camera should follow the spaceship
-            game.camera.follow(spaceship);
-
+            // Initialise the Selection module
+            selection = new Selection(game, spaceship);
             // Initialise the collisions module
             collisions = new Collisions(game, aliens, spaceship, powerUps);
+            // Initialise the cursors
+            cursors = game.input.keyboard.createCursorKeys();
         },
         update: function () {
             spaceship.update();
             collisions.update();
+
+            // Move the camera
+            if (cursors.up.isDown) {
+                game.camera.y -= 10;
+            } else if (cursors.down.isDown) {
+                game.camera.y += 10;
+            } if (cursors.left.isDown) {
+                game.camera.x -= 10;
+            } else if (cursors.right.isDown) {
+                game.camera.x += 10;
+            }
         },
         render: function () {
-            game.debug.text('FPS: ' + game.time.fps || '--', 12, 40, "#00ff00");
             game.debug.text(
                 'Health: ' + spaceship.getHealth(),
                 12,
                 20,
                 Utils.getColourForValue(spaceship.getHealth())
             );
+            game.debug.text('FPS: ' + game.time.fps || '--', 12, 40, "#00ff00");
+            game.debug.text('Selected units: ' + Utils.getSelectedUnitsSummary(game.selectedUnits), 12, 60, "#00ff00");
             // game.debug.body(spaceship);
         }
     };
